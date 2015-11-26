fa25b09d76aa61886e84bb0fc35b56abc789dd29
JelteMX <jelte.lagendijk@mendix.com>
2015-11-26 15:49:00 +0100

    First setup

---
+++ cli.js
@@ -0,0 +1,54 @@
+'use strict';
+
+var optimist = require('optimist'),
+    chalk = require('chalk'),
+    _ = require('lodash'),
+    updateNotifier = require('update-notifier'),
+    currentFolder = require('path').resolve('./') + '/',
+    pkg = require('./package.json');
+
+var Checker = require('./lib/checker');
+var banner = '\n\n  ================ Mendix Check Deprecations for Mendix 6 ================ \n\n';
+
+var argv = optimist
+	.usage([
+		' Usage : ' + chalk.bold.cyan('mx-check-deprecations [OPTIONS] [<file.mpk> <file.mpk> <file.mpk> ... ]'),
+		'',
+        ' Either check individual files, or the whole folder'
+	].join('\n'))
+	.boolean('d')
+	.alias('d', 'update-deprecations')
+		.describe('d', 'Update the list with deprecations from Github (not available yet)')
+	.alias('u', 'update')
+        .describe('u', 'Checks if there is an update for mx-check-deprecations')
+	.alias('h', 'help')
+        .describe('h', 'Shows this help screen')
+	.argv;
+
+var files = argv._;
+
+console.log(banner);
+
+if (argv.update) {
+    console.log(chalk.cyan('\n Checking for an update'));
+    updateNotifier({
+        pkg: pkg,
+        callback: function(err, update) {
+            if (err) {
+                console.log(chalk.red('\n\n Error checking the update : '), err);
+            } else {
+                if (update.latest !== update.current) {
+                    console.log(chalk.green(' Update available! Run ') + chalk.bold.cyan('npm update -g mx-check-deprecations') + chalk.green(' to install version ') + chalk.bold.cyan(update.latest) + '\n');
+                } else {
+                    console.log(chalk.green(' You are running the latest version :-)\n'));
+                }
+            }
+            process.exit(0);
+        }
+    });
+} else if (argv.help) {
+    console.log(optimist.help());
+    process.exit(0);
+} else {
+    Checker.checkFiles(files, currentFolder);
+}
